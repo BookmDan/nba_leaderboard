6ec56ec094057dafae7bedb7a5ee0b47db118fd0
BookmDan <danoh329@gmail.com>
2023-12-29 20:44:40 -0800

    set up environment complete

--- flatiron/phase4/python-p4-project/server/config.py
+++ flatiron/phase4/python-p4-project/server/config.py
@@ -28,4 +28,6 @@ db.init_app(app)
 api = Api(app)
 
 # Instantiate CORS
-CORS(app)
+# CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
---
+++ flatiron/phase4/python-p4-project/server/migrations/versions/f5a602837a7a_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: f5a602837a7a
+Revises: 
+Create Date: 2023-12-29 17:23:52.319558
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f5a602837a7a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
